50a7f0a620cacde9a0f4224189be2121d4c7878f
PetrMitsel <iampetermitsel@gmail.com>
2019-10-21 14:35:40 -0400

    Added routes to login and register form

--- app.py
+++ app.py
@@ -4,10 +4,18 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
-def main():
+@app.route("/")
+@app.route("/home")
+def home():
     return render_template('home.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
 
 
 
