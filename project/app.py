e6a963c305f79b18e1e9248816b8103b14a4004f
PetrMitsel <iampetermitsel@gmail.com>
2019-11-27 15:05:37 -0500

    set up various libraries

--- project/app.py
+++ project/app.py
@@ -1,11 +1,20 @@
-from flask import Flask,request,render_template,url_for,redirect
+from flask import Flask,request,render_template,url_for,redirect,flash
 from flask_sqlalchemy import SQLAlchemy
+from models import User,db
+
+from flask_login import login_user,current_user,logout_user,login_required,LoginManager
+from flask_bcrypt import Bcrypt
+
 
 #import dbconfig
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
-db = SQLAlchemy(app)
+db.init_app(app)
+bcrypt= Bcrypt(app)
+login_manager=LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category= 'info'
 
 @app.route("/")
 @app.route("/home")
@@ -19,22 +28,21 @@ def login():
 
 @app.route("/register", methods=['GET','POST'])
 def register():
-    if request.method== 'POST':
-        first_name=request.form['f-name']
-        last_name=request.form['l-name']
-        email=request.form['email']
-        password=request.form['password']
-        cur = mysql.connection.cursor()
-        cur.execute(''' INSERT INTO Users (teacher_firstname,teacher_lastname,teacher_email,teacher_password) 
-        VALUES (%s,%s,%s,%s)''',(first_name,last_name,email,password))
-        mysql.connection.commit()
-        cur.close()
-        return redirect(url_for('myclasses'))
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    #form= RegistrationForm ()
+    #if form.validate_on_submit():
+    #    user = User(username=form.username.data,email=form.email.data,password=hashed_password)
+    #    db.session.add(user)
+    #    db.session.commit()
+    #    flash('Account succesfully created.','success')
+    #    return redirect(url_for('login'))
     return render_template('register.html')
 
 
 @app.route("/myclasses")
 def myclasses():
+
     return render_template('myclasses.html')
 
 
--- project/models.py
+++ project/models.py
@@ -1,13 +1,15 @@
-from app import db
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
+db = SQLAlchemy()
 class User(db.Model,UserMixin):
    id = db.Column('teacher_id', db.Integer, primary_key = True)
    name = db.Column(db.String(100),nullable=True)
    username = db.Column(db.String(20),unique=True,nullable=False)
    email = db.Column(db.String(120),unique=True,nullable=False)
    password = db.Column(db.String(60),nullable=True)
+   courses = db.relationship('Course', backref='Coursetaught', lazy=True)
    
    def __repr__(self):
         return f"User('{self.username},{self.email}')"
@@ -15,5 +17,11 @@ class User(db.Model,UserMixin):
 class Course(db.Model):
     id = db.Column('course_id',db.Integer,primary_key=True)
     name= db.Column(db.String(100),unique=True,nullable=False)
-    content = db.Column(db.Text,nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+    students = db.relationship('Student', backref='Student', lazy=True)
+    teacher_id = db.Column(db.Integer, db.ForeignKey('User.id'),nullable=False)
+
+
+class Student(db.Model):
+    name = db.Column(db.String(100),nullable=True)
+    id = db.Column('student_id',db.Integer,primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('Course.id'),nullable=False)
