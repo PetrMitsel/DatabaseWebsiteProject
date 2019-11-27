ab00ab397da30ee14f2fd291a24d2810af302472
PetrMitsel <iampetermitsel@gmail.com>
2019-11-27 13:50:12 -0500

    changed structure

--- dbconfig.py
+++
@@ -1,9 +0,0 @@
-import app
-from flask_mysqldb import MySQL
-
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'school_database'
-
-mysql = MySQL(app)
