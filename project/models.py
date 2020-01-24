a5582df9199ea87aceece9480bea2330a3f12b8b
PetrMitsel <iampetermitsel@gmail.com>
2020-01-24 17:10:48 -0500

    Added models for grade,assignment

--- project/models.py
+++ project/models.py
@@ -1,4 +1,4 @@
-from project import login_manager,db
+from project import login_manager, db
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
@@ -7,33 +7,64 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(db.Model,UserMixin):
-   __tablename__ = 'users'
-   id = db.Column(db.Integer, primary_key = True)
-   firstname = db.Column(db.String(100),nullable=True)
-   lastname = db.Column(db.String(20),nullable=False)
-   email = db.Column(db.String(120),unique=True,nullable=False)
-   password = db.Column(db.String(60),nullable=True)
-   courses = db.relationship('Course', backref='Teacher', lazy=True)
 
-   def __repr__(self):
+class User(db.Model, UserMixin):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=True)
+    lastname = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=True)
+    courses = db.relationship("Course", backref="Teacher", lazy=True)
+
+    def __repr__(self):
         return f"User('{self.id},{self.email}')"
 
+
 class Course(db.Model):
-    id = db.Column('course_id',db.Integer,primary_key=True)
-    name= db.Column(db.String(100),unique=True,nullable=False)
-    students = db.relationship('Student', backref='Course', lazy=True)
-    teacher_id = db.Column(db.Integer, db.ForeignKey('users.id'),nullable=False)
+    id = db.Column("course_id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    students = db.relationship("Student", backref="Course", lazy=True)
+    teacher_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    assignments = db.relationship("Assignment", backref="Course", lazy=True)
 
     def __repr__(self):
         return self.name
 
+
 class Student(db.Model):
-    first_name = db.Column(db.String(100),nullable=True)
-    last_name = db.Column(db.String(100),nullable=True)
-    id = db.Column('student_id',db.Integer,primary_key=True)
-    course_id = db.Column(db.Integer, db.ForeignKey('course.course_id'),nullable=False)
-    homework = db.Column(db.Integer,nullable=True)
-    midterm = db.Column(db.Integer,nullable=True)
-    final = db.Column(db.Integer,nullable=True)
-    average= db.Column(db.Integer,nullable=True)
+
+    first_name = db.Column(db.String(100), nullable=True)
+    last_name = db.Column(db.String(100), nullable=True)
+    id = db.Column("student_id", db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey("course.course_id"), nullable=False)
+    grades = db.relationship("Grade", backref="Student", lazy=True)
+    homework = db.Column(db.Integer, nullable=True)
+    midterm = db.Column(db.Integer, nullable=True)
+    final = db.Column(db.Integer, nullable=True)
+    average = db.Column(db.Integer, nullable=True)
+
+
+class Grade(db.Model):
+
+    id = db.Column("grade_id", db.Integer, primary_key=True)
+    student_id = db.Column(
+        db.Integer, db.ForeignKey("student.student_id"), nullable=False
+    )
+    assignment_id = db.Column(
+        db.Integer, db.ForeignKey("student.student_id"), nullable=False
+    )
+    __table_args__ = (
+        db.UniqueConstraint("student_id", "assignment_id"),
+    )
+    value = db.Column(db.Integer, nullable=False)
+
+
+class Assignment(db.Model):
+
+    id = db.Column("assignment_id", db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey("course.course_id"), nullable=False)
+    weight = db.Column(db.Integer, nullable=False)
+    assignment_name = db.Column(db.String(100), nullable=True)
+    grades = db.relationship("Grade", backref="Assignment", lazy=True)
+
--- project/routes.py
+++ project/routes.py
@@ -1,99 +1,152 @@
-from flask import Flask,request,render_template,url_for,redirect,flash,session
+from flask import Flask, request, render_template, url_for, redirect, flash, session
 from flask_sqlalchemy import SQLAlchemy
-from project.models import User,Course,Student
-from project import db,app,bcrypt
-from project.forms import RegistrationForm,AddClassForm,AddStudentForm,LoginForm
-from flask_login import login_user,current_user,logout_user,login_required,LoginManager
+from project.models import User, Course, Student
+from project import db, app, bcrypt
+from project.forms import RegistrationForm, AddClassForm, AddStudentForm, LoginForm
+from flask_login import (
+    login_user,
+    current_user,
+    logout_user,
+    login_required,
+    LoginManager,
+)
 import os
 import sys
 import statistics
 
 
-
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route("/login",methods=['GET','POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    form= LoginForm()
+    form = LoginForm()
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for("home"))
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password,form.password.data):
-            login_user(user,remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("home"))
         else:
-            flash('Login Unsuccesful. Please check email and Password','danger')
-    return render_template('login.html',form=form)
+            flash("Login Unsuccesful. Please check email and Password", "danger")
+    return render_template("login.html", form=form)
 
-@app.route("/register", methods=['GET','POST'])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form= RegistrationForm ()
+        return redirect(url_for("home"))
+    form = RegistrationForm()
     if form.validate_on_submit():
-       hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-       new_user = User(firstname=form.first_name.data,lastname=form.last_name.data,email=form.email.data,password=hashed_password)
-       db.session.add(new_user)
-       db.session.commit()
-       flash('Account succesfully created.','success')
-       print('Hello world!', file=sys.stderr)
-       return redirect(url_for('login'))
-    return render_template('register.html',form=form)
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
+        new_user = User(
+            firstname=form.first_name.data,
+            lastname=form.last_name.data,
+            email=form.email.data,
+            password=hashed_password,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Account succesfully created.", "success")
+        print("Hello world!", file=sys.stderr)
+        return redirect(url_for("login"))
+    return render_template("register.html", form=form)
+
 
 @app.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for("home"))
 
 
-@app.route("/myclasses",methods=['GET','POST'])
+@app.route("/profile", methods=["GET", "POST"])
+@login_required
+def profile():
+    user = current_user
+    return render_template("profile.html", user=current_user, courses=getcourses())
+
+
+@app.route("/course/<course_id>", methods=["GET", "POST"])
+@login_required
+def course_detail(course_id):
+    course = Course.query.filter_by(id=course_id).first()
+    return render_template("course_detail.html", user=current_user, course=course)
+
+
+@app.route("/myclasses", methods=["GET", "POST"])
 @login_required
 def myclasses():
-    courses= getcourses()
-    addstudentform = AddStudentForm ();
-    addclassform = AddClassForm ();
-    
+    courses = getcourses()
+    addstudentform = AddStudentForm()
+    addclassform = AddClassForm()
+
     if addclassform.submitclass.data and addclassform.validate_on_submit():
-        addclasses(addclassform.class_name.data,current_user)
-        return redirect(url_for('myclasses'))
+        addclasses(addclassform.class_name.data, current_user)
+        return redirect(url_for("myclasses"))
 
-    return render_template('myclasses.html',addclassform=addclassform,addstudentform=addstudentform,courses=courses)
+    return render_template(
+        "myclasses.html",
+        addclassform=addclassform,
+        addstudentform=addstudentform,
+        courses=courses,
+    )
 
 
-@app.route("/addstudent",methods=['GET','POST'])
+@app.route("/addstudent", methods=["GET", "POST"])
 @login_required
 def addstudent():
 
-    courses= getcourses()
-    addstudentform = AddStudentForm ();
-    addclassform = AddClassForm ();
-    print(bool(addstudentform.validate_on_submit()),file=sys.stderr)
+    courses = getcourses()
+    addstudentform = AddStudentForm()
+    addclassform = AddClassForm()
+    print(bool(addstudentform.validate_on_submit()), file=sys.stderr)
 
     if addstudentform.validate_on_submit():
-        grades= [addstudentform.homework.data,addstudentform.midterm.data,addstudentform.final.data]
-        student = Student(first_name=addstudentform.students_first_name.data,last_name=addstudentform.students_last_name.data,Course=addstudentform.student_class.data,
-        homework=addstudentform.homework.data,midterm=addstudentform.midterm.data,final=addstudentform.final.data,average=statistics.mean(grades))
+        grades = [
+            addstudentform.homework.data,
+            addstudentform.midterm.data,
+            addstudentform.final.data,
+        ]
+        student = Student(
+            first_name=addstudentform.students_first_name.data,
+            last_name=addstudentform.students_last_name.data,
+            Course=addstudentform.student_class.data,
+            homework=addstudentform.homework.data,
+            midterm=addstudentform.midterm.data,
+            final=addstudentform.final.data,
+            average=statistics.mean(grades),
+        )
         print(student.first_name, file=sys.stderr)
         db.session.add(student)
         db.session.commit()
-        return redirect(url_for('myclasses'))
+        return redirect(url_for("myclasses"))
 
-    return render_template('addstudent.html',addclassform=addclassform,addstudentform=addstudentform,courses=courses)
+    return render_template(
+        "addstudent.html",
+        addclassform=addclassform,
+        addstudentform=addstudentform,
+        courses=courses,
+    )
 
-def addclasses(s,user):
-    
-    course = Course(name=s,Teacher=user)
+
+def addclasses(s, user):
+
+    course = Course(name=s, Teacher=user)
     db.session.add(course)
     db.session.commit()
 
+
 def getcourses():
-    courses= Course.query.filter_by(Teacher=current_user).all()
+    courses = Course.query.filter_by(Teacher=current_user).all()
     return courses
+
 
 def makechoices(courses):
     coursenames = []
@@ -101,10 +154,11 @@ def makechoices(courses):
     for course in courses:
         coursenames.append(course.name)
         students.append(course.students)
-    #print(students, file=sys.stderr)      
-    choices= zip(courses,coursenames)
-    choices= set(choices)
+    # print(students, file=sys.stderr)
+    choices = zip(courses, coursenames)
+    choices = set(choices)
     return choices
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
